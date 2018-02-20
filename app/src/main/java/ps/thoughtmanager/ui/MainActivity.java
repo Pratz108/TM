f0416f9cb6979f173eaf86da76d5ebb11e6ef34d
Pratiksha Singh <pratikshasingh97@gmail.com>
2018-02-20 19:16:01 +0530

    Random quotes generator

--- app/src/main/java/ps/thoughtmanager/ui/MainActivity.java
+++ app/src/main/java/ps/thoughtmanager/ui/MainActivity.java
@@ -1,13 +1,20 @@
 package ps.thoughtmanager.ui;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -15,6 +22,13 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
 
 import ps.thoughtmanager.R;
 import ps.thoughtmanager.models.User;
@@ -26,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
     FirebaseUser currentUser;
     DatabaseReference dbRef;
     int pCount, nCount;
-
+    private StorageReference mStr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +53,51 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 dbRef.getRef().child("negative").setValue(++nCount);
                 if (nCount >= pCount) {
+                    mStr = FirebaseStorage.getInstance().getReference();
+                    Random r = new Random(5);
+                    int n = r.nextInt(5);
+                    Log.d(getLocalClassName(), "the value of n is " + n );
+                    StorageReference cRef = mStr.child("quotes/" + n + ".jpeg");
+                    try {
+                        final File localFile = File.createTempFile("images", "jpeg");
+                        cRef.getFile(localFile)
+                                .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                                    @Override
+                                    public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+
+                                        if(localFile.exists()){
+
+                                            Bitmap myBitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+
+                                            final ImageView myImage = (ImageView) findViewById(R.id.image);
+
+                                            myImage.setImageBitmap(myBitmap);
+                                            new Handler().postDelayed(new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                    myImage.setImageBitmap(null);
+                                                }
+                                            },1000);
+
+
+                                        }
+
+
+                                        // Successfully downloaded data to local file
+                                        // ...
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception exception) {
+                                // Handle failed download
+                                // ...
+                            }
+                        });
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+
                     Toast.makeText(MainActivity.this, "Smile na Bro..!!", Toast.LENGTH_LONG).show();
                 }
             }
